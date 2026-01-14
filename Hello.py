19c18e44051a40c68c20d74d31f8555104da62d3
Abdul Adnan <peerabduladnan@gmail.com>
2026-01-14 15:50:29 +0530

    Made first addtion

--- Hello.py
+++ Hello.py
@@ -2,3 +2,22 @@ print('Python is running in vsCode')
 
 name = input("Enter your name")
 print("Hello", name)
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+op = input("Choose operation (+, -, *, /): ")
+
+if op == "+" : 
+    print("Result: ", num1 + num2)
+elif op == "-":
+    print("Result: ", num1 - num2)
+elif op == "*": 
+    print("Result: ", num1 * num2)
+elif op == "/":
+    if num2 != 0:
+        print("Result:", num1/ num2)
+    else: print("Error: cannot divide by zero")
+
+else: 
+    print("Invalid operation")
