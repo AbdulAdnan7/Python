ad1f44002368f52a46343d7d88e49055296c5140
Abdul Adnan <peerabduladnan@gmail.com>
2026-01-17 03:14:36 +0530

    learned how to write input in python

--- basics/basic.py
+++ basics/basic.py
@@ -1,18 +1,29 @@
-# print("Line 1")
+# # print("Line 1")
 
-# #data types
-# print("words") # stirng
-# print("words")
-# print(12344) # integers / ints
-# print(-939392)
-# print(1.5) # floating point value
+# # #data types
+# # print("words") # stirng
+# # print("words")
+# # print(12344) # integers / ints
+# # print(-939392)
+# # print(1.5) # floating point value
 
 
-#operations
-print(3 + 3) # addittion
-print(3 - 3) # Subtraction
-print(3 * 3) # Multiplication
-print(3 / 3) # / division
-print(3 + 3.33223) 
-# print(10 + "Hello") #  got type error
-print(10 * 'hello') #got hello 10xtimes
+# #operations
+# print(3 + 3) # addittion
+# print(3 - 3) # Subtraction
+# print(3 * 3) # Multiplication
+# print(3 / 3) # / division
+# print(3 + 3.33223) 
+# # print(10 + "Hello") #  got type error
+# print(10 * 'hello') #got hello 10xtimes
+
+# # VARIABLES
+# # // Values //
+
+# test  = 234
+# test = test + 1000
+# test += 1000
+# print(test - 200)
+
+user = int(input("please write something: "))
+print(user)
