28021bfa55643ea70b9ca2badea05f4f82d3b175
Abdul Adnan <peerabduladnan@gmail.com>
2026-01-16 13:19:49 +0530

    second one

--- third.py
+++ third.py
@@ -1,6 +1,15 @@
 age = 19
 
+marks = 20
+
 if age >= 18:
     print("You are an adult")
 else: 
    print("you are a minor")
+
+if marks >= 90:
+    print("A grade")
+elif marks >= 70:
+    print("B grade")
+else:  
+    print("C grade")   
